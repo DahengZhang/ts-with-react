7082409a58474ff176e1ff5aa0e392b1cba1a084
张大亨 <zhangdaheng@zhangdahengdeMacBook-Pro.local>
2020-06-28 23:40:53 +0800

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello'
@@ -8,8 +8,13 @@ import useMousePosition from './hooks/useMousePosition'
 
 function App() {
   const positions = useMousePosition()
+  const inputDomRef = useRef<HTMLInputElement>(null)
+  useEffect(() => {
+    inputDomRef?.current?.focus()
+  }, [])
   return (
     <div className="App">
+      <input type="text" ref={inputDomRef}/>
       <Hello message={'hello world'} />
       <p>X: {positions.x}, Y: {positions.y}</p>
       <LikeButton />
--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,19 +1,15 @@
 import React, { useState, useEffect } from 'react'
 
-import useMousePosition from '../hooks/useMousePosition'
-
 const LikeButton: React.FC = () => {
     const [like, setLike] = useState(0)
-    const positions = useMousePosition()
 
     useEffect(() => {
         // 组件渲染时渲染
         document.title = `点击了${like}`
-    })
+    }, [like])
 
     return (
         <>
-            <p>X: {positions.x}, Y: {positions.y}</p>
             <button onClick={() => {setLike(like + 1)}}>👍{like}</button>
         </>
     )
